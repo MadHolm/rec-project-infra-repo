a4f3cf5a9d077422cc36b157e4ffa6e1eec0444a
Artur <35669709+MadHolm@users.noreply.github.com>
2024-11-04 14:23:50 +0100

    Create main.tf

---
+++ infra/main.tf
@@ -0,0 +1,15 @@
+# main.tf
+
+provider "google" {
+  project = "rec-project-gcp"   # Project ID where the bucket will be created
+}
+
+resource "google_storage_bucket" "test_bucket" {
+  name     = "my-test-bucket-rec-project-gcp"  # Unique name for the bucket
+  location = "EU"                              # GCS location (US, EU, or a specific region like us-central1)
+}
+
+# Outputs (optional but helpful to confirm)
+output "bucket_name" {
+  value = google_storage_bucket.test_bucket.name
+}
