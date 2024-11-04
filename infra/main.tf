77f6b8e204dd11bb73457682dc1b7683e7345cf7
Artur <35669709+MadHolm@users.noreply.github.com>
2024-11-04 14:48:03 +0100

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -9,6 +9,11 @@ resource "google_storage_bucket" "test_bucket" {
   location = "EU"                              # GCS location (US, EU, or a specific region like us-central1)
 }
 
+resource "google_storage_bucket" "test_bucket_2" {
+  name     = "my-second-test-bucket-rec-project-gcp"  # Unique name for the bucket
+  location = "EU"                                     # GCS location (US, EU, or a specific region like us-central1)
+}
+
 # Outputs (optional but helpful to confirm)
 output "bucket_name" {
   value = google_storage_bucket.test_bucket.name
